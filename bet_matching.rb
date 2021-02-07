d66cfb0c984a9ba5436fa279b78a077c1bb186bc
lareb nawab <lareb.indore@gmail.com>
2021-02-07 20:32:07 +0530

    Buetify logs

--- bet_matching.rb
+++ bet_matching.rb
@@ -40,14 +40,12 @@ class BetMatching < GenerateBets
     # ap bets
     backs = bets.select{ |b| b[:type] == :back }
     lays = bets.select{ |b| b[:type] == :lay }
-    puts "----backs = #{backs.length} and --- lays = #{lays.length}"
+    puts "----backs = #{backs.length} and --- lays = #{lays.length}\n================="
 
     total_back_unmatched_amount = backs.map{|t| t[:unmatched_amount]}.sum
     total_lay_unmatched_amount = lays.map{|t| t[:unmatched_amount]}.sum
-    @log.info "\n-----All back bets count => (#{backs.length})\nTotal back amount = #{total_back_unmatched_amount}------"
-    @log.info JSON.pretty_generate(backs)
-    @log.info "\n-----All lay bets count => (#{lays.length})\nTotal lay amount = #{total_lay_unmatched_amount}------"
-    @log.info JSON.pretty_generate(lays)
+    @log.info "\n-------------------\n#{backs.length} back bets in total\nTotal back amount = #{total_back_unmatched_amount}\n\n#{JSON.pretty_generate(backs)}"
+    @log.info "\n-------------------\n#{lays.length} lay bets in total\nTotal lay amount = #{total_lay_unmatched_amount}\n\n#{JSON.pretty_generate(lays)}"
 
     match_bets(backs, lays)
   end
@@ -62,6 +60,8 @@ class BetMatching < GenerateBets
       secondary_bets = backs
     end
 
+    final_matched_bet = []
+
     # iterate bets
     primary_bets.each_with_index do |bet, index|
       # primary bet is matched
@@ -69,11 +69,11 @@ class BetMatching < GenerateBets
       current_bet_odds = bet[:odds]
       # primary_bet_unmatched_amount = bet[:unmatched_amount]
 
-      puts "#{index}. current bet type is => #{bet[:type]}\ncurrent odds is ===========#{current_bet_odds}\nunmatched amount => #{bet[:unmatched_amount]}"
+      puts "current bet type is => #{bet[:type]}\ncurrent odds is ===========#{current_bet_odds}\nunmatched amount => #{bet[:unmatched_amount]}\nPrimary bet id = #{bet[:id]}"
       # get similar odds bets
       similar_odds_bets = secondary_bets.select{ |b| b[:odds] == current_bet_odds && b[:status] != 'fully_matched' }
-      ap similar_odds_bets
-      puts "---------------matching explained below------------------\n"
+      # ap similar_odds_bets
+      # puts "---------------matching explained below------------------\n"
 
       primary_matched_with_opposit_bets = []
       similar_odds_bets.each do |matching_with_bet|
@@ -82,6 +82,7 @@ class BetMatching < GenerateBets
         if(matching_with_bet[:unmatched_amount] == bet[:unmatched_amount])
           # both current bet and matching bet will adjust, both will update will status matched with 0 un matched amount
           # in this case exit the loop
+          puts "Primary bet amount is FULLY_MATCHED with id/amount #{matching_with_bet[:id]}/#{matching_with_bet[:unmatched_amount]}\nMatched with bet is also FULLY_MATCHED"
           bet[:unmatched_amount] = 0
           bet[:status] = "fully_matched"
           matching_with_bet[:unmatched_amount] = 0
@@ -93,37 +94,47 @@ class BetMatching < GenerateBets
           # current bet will be partially settle
           # break the loop
           bet[:unmatched_amount] = bet[:unmatched_amount] - matching_with_bet[:unmatched_amount]
+          puts "Primary bet amount is PARTIALLY_MATCHED with id/amount #{matching_with_bet[:id]}/#{matching_with_bet[:unmatched_amount]}\nUnmatched amount is = #{bet[:unmatched_amount]}\nMatched with bet is also FULLY_MATCHED"
           bet[:status] = "partially_matched"
           matching_with_bet[:unmatched_amount] = 0
-          matching_with_bet[:status] = "fuly_matched"
+          matching_with_bet[:status] = "fully_matched"
         elsif(bet[:unmatched_amount] < matching_with_bet[:unmatched_amount])
           # current bet will settle
           # matching with bet remain partial_settle
           matching_with_bet[:unmatched_amount] = matching_with_bet[:unmatched_amount] - bet[:unmatched_amount]
+          puts "Primary bet amount is FULLY_MATCHED with id/amount #{matching_with_bet[:id]}/#{matching_with_bet[:unmatched_amount]}\nMatched with bet is PARTIALLY_MATCHED with amount #{matching_with_bet[:unmatched_amount]}"
           matching_with_bet[:status] = "partially_matched"
           bet[:unmatched_amount] = 0
           bet[:status] = "fully_matched"
           primary_bet_matched = true
         end
-        puts "primary bet's amount left = #{bet[:unmatched_amount]}"
+        # puts "primary bet's amount left = #{bet[:unmatched_amount]}"
         # push settle bets against primary bet
         primary_matched_with_opposit_bets.push(matching_with_bet)
+        final_matched_bet.push([bet[:id], matching_with_bet[:id], current_bet_odds].join("  |  "))
         break if primary_bet_matched
       end
 
-      @log.info "\n\n\n<<<<<<<<<<<<<<Matching details>>>>>>>>>>>>>>>>>\n"
-      @log.info JSON.pretty_generate(bet)
-      @log.info "above bet is matched with following bets (beow)"
-      @log.info JSON.pretty_generate(primary_matched_with_opposit_bets)
-      @log.info "\n\n\n\n\++++++++++++++++++++++++++++++++++++++++++++++++++\n\n\n\n"
-
-      puts "settled with bets are following ==============="
-      ap primary_matched_with_opposit_bets
-
-      puts "-------------END---------------\n\n"
+      @log.info "\n\n\n<<<<<<<<<<<<<<Matching bet #{bet[:id]}>>>>>>>>>>>>>>>>>\n#{bet}\nabove bet is matched with following bets (beow)\n#{JSON.pretty_generate(primary_matched_with_opposit_bets)}"
+      # @log.info primary_matched_with_opposit_bets
+      @log.info "\n>>>>>>>>>>>>>>>>>Matching of bet #{bet[:id]} is completed<<<<<<<<<<<<<<<<<<\n\n"
       # remove below break, its only for debuggin one record
       # break
     end
+
+    logger_trace "------------Final matched bets-----------"
+    ap final_matched_bet
+    @log.info "\n   bet_id |m_bet_id | odds\n------------------------------\n#{JSON.pretty_generate(final_matched_bet)}"
+    logger_trace "------------Bets after match-----------"
+    sorted_final_bets = primary_bets.concat(secondary_bets).sort{|x, y| x[:status] <=> y[:status]}.map{|x| [x[:id], x[:odds], x[:unmatched_amount], x[:status]].join("    |   ")  }
+    @log.info "\n   bet_id   |  odds   | status  | unmatched_amount\n------------------------------\n#{JSON.pretty_generate(sorted_final_bets)}"
+    ap sorted_final_bets
+
+  end
+
+  def logger_trace(str)
+    @log.info (str)
+    ap str
   end
 
   # get the sample data, this will create a new set of sample data at every run
